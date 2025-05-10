99fa3d8a482d82626004f3453054134279f4f73a
Marcelo Mogrovejo <mogro@Marcelos-MacBook-Pro.local>
2025-05-10 13:56:24 +0800

    WIP - Added a better approach to trigger the authentication.

--- LoginDemo.SwiftUI/Scenes/Authentication/ViewModels/SignInViewModel.swift
+++ LoginDemo.SwiftUI/Scenes/Authentication/ViewModels/SignInViewModel.swift
@@ -16,6 +16,7 @@ class SignInViewModel: ObservableObject {
     @Published var password: String = ""
     // Output subscriber
     @Published var isFormValid: Bool = false
+    @Published var shouldAuthenticate = false
 
     private var cancellables: Set<AnyCancellable> = []
     var appSettings: AppSettings?
@@ -25,17 +26,55 @@ class SignInViewModel: ObservableObject {
             .receive(on: RunLoop.main)
             .assign(to: \.isFormValid, on: self)
             .store(in: &cancellables)
+
+        $shouldAuthenticate
+            // Only authenticate if shouldAuthenticate is true ($0) AND the form is valid
+            .filter { $0 && self.isFormValid }
+            // It is the crucial operator that allows you to "subscribe" to a publisher and
+            // define what actions should occur whenever the publisher emits a new value or
+            // when it finishes. It's the bridge between the reactive data flow you define
+            // with Combine and the imperative actions you need to perform in your application.
+            .sink { _ in
+                self.authenticateUser()
+                // Reset the trigger
+                self.shouldAuthenticate = false
+            }
+            .store(in: &cancellables)
+    }
+
+    func triggerAuthentication() {
+        shouldAuthenticate = true
+    }
+
+//    func validateForm() {
+        // This is working but it is not robust bocause it depends
+        // on SwiuftUI way to observe published vars.
+        // Even though validateForm() itself doesn't directly change
+        // email or password, the fact that you're calling a method
+        // on the ViewModel within the UI event handler (onSubmit())
+        // could be enough for SwiftUI's change detection to kick in.
+        // It might be thinking: "The user interacted with the UI and
+        // called a ViewModel method; I should probably check if any
+        // @Published properties that the UI depends on have been
+        // affected (even indirectly)."
+        // Important Note: Relying on an empty method to trigger a
+        // Combine pipeline update is generally not a robust or
+        // recommended practice. It's a bit of a side effect of
+        // SwiftUI's observation mechanism. The more reliable way to
+        // ensure your validation runs is when the actual @Published
+        // properties (email and password) change due to user input.
+//        print("ViewModel validateForm()...")
+//    }
+
+    private func authenticateUser() {
+        print("Authenticating...")
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) { [weak self] in
+            self?.appSettings?.isLoggedIn = true
+        }
     }
 
     func setup(_ appSettings: AppSettings) {
         self.appSettings = appSettings
-    }
-
-    func authenticate() {
-        appSettings?.isLoggedIn = true
-//        DispatchQueue.main.asyncAfter(deadline: .now() + 2) { [weak self] in
-//            self?.settings.isLoggedIn = true
-//        }
     }
 }
 
